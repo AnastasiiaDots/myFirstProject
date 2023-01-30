5c7b866fa90ad157c03137e5734084a59f9e1b29
Anastasiia Dotsenko <anastasiia_dots@yahoo.com>
2023-01-31 09:37:54 +1300

    console.log prices

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const title = "First Project"
 console.log(typeof title)
 const screens = "Простые, Сложные, Интерактивные"
+console.log(screens.length)
 const screenPrice = 230
 const rollback = 10
-const fullPrice = 250
+const fullPrice = 500
+console.log(screenPrice && fullPrice)
 console.log(typeof fullPrice)
 const adaptive = true
 console.log(typeof adaptive)
