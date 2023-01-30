f3b2b0363aef94a0686361a9bea177d3586007c4
Anastasiia Dotsenko <anastasiia_dots@yahoo.com>
2023-01-30 15:34:16 +1300

    edited script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+alert("Please check my work!")
+const title = "First Project"
+const screens = "2"
+const screenPrice = "230"
+const rollback = "10"
+const fullPrice = "250"
+const adaptive = "true"
+console.log("Hi mate!")
