a70ec76a118a2d142ec5839defd379aae2978e78
Anastasiia Dotsenko <anastasiia_dots@yahoo.com>
2023-01-30 22:15:22 +1300

    added value for const

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-alert("Please check my work!")
 const title = "First Project"
-const screens = "2"
+const screens = "Простые, Сложные, Интерактивные"
 const screenPrice = "230"
 const rollback = "10"
 const fullPrice = "250"
 const adaptive = "true"
-console.log("Hi mate!")
