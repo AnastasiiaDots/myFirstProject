263b65bc5a37c6997b779d60ccf6771fa045ec7a
Anastasiia Dotsenko <anastasiia_dots@yahoo.com>
2023-01-31 20:47:10 +1300

    replaed console.log

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ const fullPrice = 500;
 const adaptive = true;
 
 console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
 console.log(screens.length);
 console.log(screens.toLowerCase());
 console.log(screens.split(", "));
 console.log(screenPrice && fullPrice);
-console.log(fullPrice * (rollback / 100));
-console.log(typeof fullPrice);
-console.log(typeof adaptive)
+console.log(fullPrice * (rollback / 100))
