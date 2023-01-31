2be0b36440ab3d703191345b357fff296f67c3b6
Anastasiia Dotsenko <anastasiia_dots@yahoo.com>
2023-02-01 08:29:24 +1300

    const lowScreens and console.log for prices

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const title = 'First Project';
 const screens = 'Простые, Сложные, Интерактивные';
+const lowScreens = screens.toLowerCase();
 const screenPrice = 230;
 const rollback = 10;
 const fullPrice = 500;
@@ -9,7 +10,13 @@ console.log(typeof title);
 console.log(typeof fullPrice);
 console.log(typeof adaptive);
 console.log(screens.length);
-console.log(screens.toLowerCase());
-console.log(screens.split(", "));
-console.log(screenPrice && fullPrice);
+
+console.log(lowScreens.split(", "));
+
+console.log("Стоимость верстки экранов", screenPrice, "$");
+console.log("Стоимость разработки сайта", fullPrice, "$");
+
 console.log(fullPrice * (rollback / 100))
+
+let num = (266219).toString();
+console.log(num[0] * num[1] * num[2] * num[3] * num[4] * num[5]);
