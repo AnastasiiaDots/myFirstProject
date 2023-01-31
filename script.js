dd9abb7d9b5b40e6cd82faf795efa26df9805a30
Anastasiia Dotsenko <anastasiia_dots@yahoo.com>
2023-01-31 20:45:23 +1300

    changed structure of code

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-const title = "First Project"
-console.log(typeof title)
-const screens = "Простые, Сложные, Интерактивные"
-console.log(screens.length)
-console.log(screens.toLowerCase())
-console.log(screens.split(", "))
-const screenPrice = 230
-const rollback = 10
-const fullPrice = 500
-console.log(screenPrice && fullPrice)
-console.log(fullPrice * (rollback / 100))
-console.log(typeof fullPrice)
-const adaptive = true
+const title = 'First Project';
+const screens = 'Простые, Сложные, Интерактивные';
+const screenPrice = 230;
+const rollback = 10;
+const fullPrice = 500;
+const adaptive = true;
+
+console.log(typeof title);
+console.log(screens.length);
+console.log(screens.toLowerCase());
+console.log(screens.split(", "));
+console.log(screenPrice && fullPrice);
+console.log(fullPrice * (rollback / 100));
+console.log(typeof fullPrice);
 console.log(typeof adaptive)
