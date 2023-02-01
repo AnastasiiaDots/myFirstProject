acc094aa8901f1f7d317930e1f57c5f0d438bee6
Anastasiia Dotsenko <anastasiia_dots@yahoo.com>
2023-02-01 22:07:28 +1300

    final version lesson02

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const title = 'First Project';
 const screens = 'Простые, Сложные, Интерактивные';
-const lowScreens = screens.toLowerCase();
 const screenPrice = 230;
 const rollback = 10;
 const fullPrice = 500;
@@ -11,9 +10,10 @@ console.log(typeof fullPrice);
 console.log(typeof adaptive);
 console.log(screens.length);
 
-console.log(lowScreens.split(", "));
+console.log(screens.toLowerCase().split(", "));
 
 console.log("Стоимость верстки экранов", screenPrice, "$");
 console.log("Стоимость разработки сайта", fullPrice, "$");
 
 console.log(fullPrice * (rollback / 100))
+
